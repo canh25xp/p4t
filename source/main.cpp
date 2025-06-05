e4a0b6d47be004579df52f2b52d4740975297733
Ngô Văn Cảnh <vancanh-ng@samsung.com>
2025-06-05 13:51:56 +0700

    add format rule

--- source/main.cpp
+++ source/main.cpp
@@ -4,80 +4,74 @@
  * This file is part of the p4api distribution package.
  *
  * This barebones example simply mimics the regular p4 command line
- * program.  
+ * program.
  *
  * Generally, compiling with the C++ compiler and linking with the
  * three provided libraries is sufficient to build this sample program.
- * 
+ *
  * See the Perforce C/C++ API User's Guide for further information.
  * https://www.perforce.com/manuals/p4api/Content/P4API/Home-p4api.html
  *
  * $Id: //depot/r25.1/p4/api/p4api.cc#1 $
  */
 
-# include "clientapi.h"
-# include "p4libs.h"
+#include "clientapi.h"
+#include "p4libs.h"
 
-int main( int argc, char **argv );
-int main( int argc, char **argv )
-{
-	ClientUser ui;
-	ClientApi client;
-	StrBuf msg;
-	Error e;
+int main(int argc, char **argv);
+int main(int argc, char **argv) {
+    ClientUser ui;
+    ClientApi client;
+    StrBuf msg;
+    Error e;
 
-	P4Libraries::Initialize( P4LIBRARIES_INIT_ALL, &e );
+    P4Libraries::Initialize(P4LIBRARIES_INIT_ALL, &e);
 
-	if( e.Test() )
-	{
-	    e.Fmt( &msg );
-	    fprintf( stderr, "%s\n", msg.Text() );
-	    return 1;
-	}
+    if (e.Test()) {
+        e.Fmt(&msg);
+        fprintf(stderr, "%s\n", msg.Text());
+        return 1;
+    }
 
-	// Any special protocol mods
+    // Any special protocol mods
 
-	// client.SetProtocol( "tag" );
+    // client.SetProtocol( "tag" );
 
-	// Enable client-side Extensions
-	// client.EnableExtensions();
+    // Enable client-side Extensions
+    // client.EnableExtensions();
 
-	// Connect to server
+    // Connect to server
 
-	client.Init( &e );
+    client.Init(&e);
 
-	if( e.Test() )
-	{
-	    e.Fmt( &msg );
-	    fprintf( stderr, "%s\n", msg.Text() );
-	    return 1;
-	}
+    if (e.Test()) {
+        e.Fmt(&msg);
+        fprintf(stderr, "%s\n", msg.Text());
+        return 1;
+    }
 
+    // Run the command "argv[1] argv[2...]"
 
-	// Run the command "argv[1] argv[2...]"
+    client.SetArgv(argc - 2, argv + 2);
+    client.Run(argv[1], &ui);
 
-	client.SetArgv( argc - 2, argv + 2 );
-	client.Run( argv[1], &ui );
+    // Close connection
 
-	// Close connection
+    client.Final(&e);
 
-	client.Final( &e );
+    if (e.Test()) {
+        e.Fmt(&msg);
+        fprintf(stderr, "%s\n", msg.Text());
+        return 1;
+    }
 
-	if( e.Test() )
-	{
-	    e.Fmt( &msg );
-	    fprintf( stderr, "%s\n", msg.Text() );
-	    return 1;
-	}
+    P4Libraries::Shutdown(P4LIBRARIES_INIT_ALL, &e);
 
-	P4Libraries::Shutdown( P4LIBRARIES_INIT_ALL, &e );
+    if (e.Test()) {
+        e.Fmt(&msg);
+        fprintf(stderr, "%s\n", msg.Text());
+        return 1;
+    }
 
-	if( e.Test() )
-	{
-	    e.Fmt( &msg );
-	    fprintf( stderr, "%s\n", msg.Text() );
-	    return 1;
-	}
-	
-	return 0;
+    return 0;
 }
